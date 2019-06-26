de75711c729a6d3fe8165a04f3935012fae13c62
liangxiegame <liangxiegame@163.com>
2019-06-26 22:14:12 +0800

    v0.0.1 环境准备

---
+++ Assets/Scripts/App.cs
@@ -0,0 +1,21 @@
+﻿using Unity.UIWidgets.engine;
+using Unity.UIWidgets.material;
+using Unity.UIWidgets.painting;
+using Unity.UIWidgets.ui;
+using Unity.UIWidgets.widgets;
+
+namespace NotePro
+{
+    public class App : UIWidgetsPanel
+    {
+        protected override Widget createWidget()
+        {
+            return new Text("Hello World",
+                style: new TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                )
+            );
+        }
+    }
+}
